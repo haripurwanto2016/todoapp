a5c35407d7f835139aaab7b4daab85ef568e0bd1
haripurwanto2016 <purwanto.hari@gmail.com>
2023-05-25 21:24:45 +0700

    Add files via upload

--- src/ToDoCard.svelte
+++ src/ToDoCard.svelte
@@ -1,7 +1,7 @@
 <script>
     import { createEventDispatcher } from "svelte";
     import { dataset_dev } from "svelte/internal";
-    
+
     export let content, listName, index;
     const dispatch = createEventDispatcher();
     function handleDeleteCard() {
@@ -22,25 +22,29 @@
         <div class="card-content">
             <div class="columns">
                 <div class="column is-1">
-                    {#if listName != 'Rencana'}
-                    <span class="icon has-text-primary" on:click={handleMoveLeft}>
-                        <i class="fa fa-chevron-left"></i>
-                    </span>
+                    {#if listName != "Rencana"}
+                        <span
+                            class="icon has-text-primary"
+                            on:click={handleMoveLeft}
+                        >
+                            L
+                        </span>
                     {/if}
                 </div>
 
                 <div class="column is-9">{content}</div>
 
                 <div class="column is-1" on:click={handleDeleteCard}>
-                    <span class="icon has-text-danger">
-                        <i class="fa fa-trash"></i>
-                    </span>
+                    <span class="icon has-text-danger"> D </span>
                 </div>
 
                 <div class="column is-1">
                     {#if listName != "Selesai"}
-                        <span class="icon has-text-primary" on:click={handleMoveRight}>
-                            <i class="fa fa-chevron-right"></i>
+                        <span
+                            class="icon has-text-primary"
+                            on:click={handleMoveRight}
+                        >
+                            R
                         </span>
                     {/if}
                 </div>
